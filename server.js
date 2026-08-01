1739a6e25bb12989dc8b11c1da81aa69b6c590c7
matontim <linniesage@gmail.com>
2026-08-01 17:04:45 -0400

    feat: add user registration and login routes

--- server.js
+++ server.js
@@ -11,3 +11,34 @@ app.get('/test', function(req, res) {
 app.listen(3000, function() {
     console.log('StudyDesk server running on http://localhost:3000');
 });
+
+const fs = require('fs');
+
+app.post('/register', function(req, res) {
+    const { username, password } = req.body;
+    const users = JSON.parse(fs.readFileSync('users.json'));
+
+    const exists = users.find(function(u) { return u.username === username; });
+    if (exists) {
+        return res.json({ success: false, message: 'Username already taken' });
+    }
+
+    users.push({ username, password });
+    fs.writeFileSync('users.json', JSON.stringify(users));
+    res.json({ success: true, message: 'Account created!' });
+});
+
+app.post('/login', function(req, res) {
+    const { username, password } = req.body;
+    const users = JSON.parse(fs.readFileSync('users.json'));
+
+    const user = users.find(function(u) {
+        return u.username === username && u.password === password;
+    });
+
+    if (user) {
+        res.json({ success: true, message: 'Welcome, ' + username + '!' });
+    } else {
+        res.json({ success: false, message: 'Invalid username or password' });
+    }
+});
